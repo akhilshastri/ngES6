b2e35bc9f788b3e488d0a767be160c2e4e0b59ec
Akhil Shastri <shastri.akhil@outlook.com>
2016-09-13 01:07:07 +0530

    server call integrated

--- app/views/search/index.js
+++ app/views/search/index.js
@@ -13,48 +13,53 @@ import 'components/searchBox';
 import 'components/searchResult';
 
 @controller()
-@inject('$scope', '$injector', '$state', '$stateParams', '$rootScope','FlightsDataService')
+@inject('$scope', '$injector', '$state', '$stateParams', '$rootScope', 'FlightsDataService')
 class ViewController extends BaseController {
     constructor() {
         super(arguments);
-      //  this.init();
-        this.model={
-            errorDictionary:{
-                'requireddestination':'Destination is require',
-                'requiredorigin':'Origin is require',
-                'requirednoOfPassengers':'No of passengers is require',
-                'requiredjurny-date':'Journey date is require',
-                'requiredreturn-date':'Return date is require',
-                "minlengthorigin" : "Minimum length for Origin is 3",
-                "minlengthdestination" : "Minimum length for Destination is 3",
-                "maxlengthorigin" : "Maximum length for Origin is 8",
-                "maxlengthdestination" : "Maximum length for Destination is 8"
+        //  this.init();
+        this.model = {
+            errorDictionary: {
+                'requireddestination': 'Destination is require',
+                'requiredorigin': 'Origin is require',
+                'requirednoOfPassengers': 'No of passengers is require',
+                'requiredjurny-date': 'Journey date is require',
+                'requiredreturn-date': 'Return date is require',
+                "minlengthorigin": "Minimum length for Origin is 3",
+                "minlengthdestination": "Minimum length for Destination is 3",
+                "maxlengthorigin": "Maximum length for Origin is 8",
+                "maxlengthdestination": "Maximum length for Destination is 8"
             },
-            search:{
-                origin:'',
-                destination:'',
-                isReturn:false
+            search: {
+                origin: '',
+                destination: '',
+                isReturn: false
             },
-            flightResult:[]
+            flightResult: []
         }
     }
-    onResetClick(){
+
+    onResetClick() {
         var conf = confirm('This will clear the search form');
-        if(conf){
-            if(this.form && this.form.ngForm){
+        if (conf) {
+            if (this.form && this.form.ngForm) {
                 this.form.ngForm.$setPristine();
             }
-            this.model.search={isReturn:false};
-            this.model.flightResult=[];
+            this.model.search = {isReturn: false};
+            this.model.flightResult = [];
         }
     }
-    onSubmitClick(){
-        alert('submit clicked');
-        this.fds.findFlights({origin:'pune',rtn:'true'}).then((result)=>{
-            this.model.flightResult=[1,2,3];
 
+    onSubmitClick() {
+        this.fds.findFlights({origin: 'pune', rtn: 'true'}).then((result)=> {
+            this.model.flightResult = [1, 2, 3];
+            setTimeout(()=> {
+                var el = $('.search-title').get(0);
+                if (el)el.scrollIntoView();
+            }, 100);
         });
     }
+
     init() {   // default init
         this.model = {
             readonly: true,
@@ -129,7 +134,7 @@ class ViewController extends BaseController {
     }
 
     get mapDI() {
-        return ['$scope', '$ig', '$state', 'sparam', '$rs','fds'];
+        return ['$scope', '$ig', '$state', 'sparam', '$rs', 'fds'];
     }
 
 }
