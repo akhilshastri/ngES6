0e69016f26a52e99640860d986028a9b4a77f9b3
Akhil Shastri <shastri.akhil@outlook.com>
2016-09-13 01:09:12 +0530

    cleanup

--- app/views/search/index.js
+++ app/views/search/index.js
@@ -17,26 +17,7 @@ import 'components/searchResult';
 class ViewController extends BaseController {
     constructor() {
         super(arguments);
-        //  this.init();
-        this.model = {
-            errorDictionary: {
-                'requireddestination': 'Destination is require',
-                'requiredorigin': 'Origin is require',
-                'requirednoOfPassengers': 'No of passengers is require',
-                'requiredjurny-date': 'Journey date is require',
-                'requiredreturn-date': 'Return date is require',
-                "minlengthorigin": "Minimum length for Origin is 3",
-                "minlengthdestination": "Minimum length for Destination is 3",
-                "maxlengthorigin": "Maximum length for Origin is 8",
-                "maxlengthdestination": "Maximum length for Destination is 8"
-            },
-            search: {
-                origin: '',
-                destination: '',
-                isReturn: false
-            },
-            flightResult: []
-        }
+          this.init();
     }
 
     onResetClick() {
@@ -62,75 +43,24 @@ class ViewController extends BaseController {
 
     init() {   // default init
         this.model = {
-            readonly: true,
-            tools: DEFA_TOOLS,
-            items: []
-        };
-        debugger;
-        const {id, readonly} = this.sparam;
-        if (id < 0) {
-            if (this.$rs.model.user) {
-                this.model.items = [];
-                this.model.readonly = false;
-            }
-        } else {
-            this.sds.get(id).then((sketch)=> {
-                const model = this.model;
-                model.items = SketchModel.parseList(sketch.items || []);
-                if (this.$rs.model.user && sketch.author == this.$rs.model.user) {
-                    model.readonly = false;
-                }
-            });
+            errorDictionary: {
+                'requireddestination': 'Destination is require',
+                'requiredorigin': 'Origin is require',
+                'requirednoOfPassengers': 'No of passengers is require',
+                'requiredjurny-date': 'Journey date is require',
+                'requiredreturn-date': 'Return date is require',
+                "minlengthorigin": "Minimum length for Origin is 3",
+                "minlengthdestination": "Minimum length for Destination is 3",
+                "maxlengthorigin": "Maximum length for Origin is 8",
+                "maxlengthdestination": "Maximum length for Destination is 8"
+            },
+            search: {
+                origin: '',
+                destination: '',
+                isReturn: false
+            },
+            flightResult: []
         }
-        this.tds.getAll().then(tools=> {
-            if (tools && tools.length > 0)   this.model.tools = tools;
-        })
-
-    }
-
-    save() {
-        const title = window.prompt("Please enter title", "New Sketch");
-        if (!title) return;
-        const author = this.$rs.model.user;
-        const items = this.model.items;
-        const id = this.sparam.id;
-        if (id < 0) {
-            this.sds.insert({title, author, items})
-                .then(()=>this.$state.go('sketches'));
-        } else {
-            this.sds.update(id, {title, author, items})
-                .then(()=>this.$state.go('sketches'));
-        }
-
-    }
-
-    attrChanged(arg, {key, selectedValue}) {
-        debugger;
-        var item = this.model.items[this.model.selectedItem];
-        item.attrs[key] = selectedValue;
-    }
-
-    activeItemChanged(arg, data) {
-        this.cleanActiveItem();
-        const model = this.model;
-        const toolName = model.items[data.index].name;
-        model.items[data.index].active = true;
-        model.activeAttrs = model.tools.find(i=>i.name == toolName).supportedAttr;
-        model.selectedItem = data.index;// model.items[];
-    }
-
-    toolDropped(arg, tool) {
-        this.cleanActiveItem();
-        tool.active = true;
-        tool.attrs = {};
-        delete tool.supportedAttr;
-        this.model.items.push(new SketchModel(tool));
-        this.$scope.$emit('active-item-changed', {index: this.model.items.length - 1, itm: tool});
-    }
-
-    cleanActiveItem() {
-        const activeItem = this.model.items.find(i=>i.active);
-        if (activeItem) activeItem.active = false;
     }
 
     get mapDI() {
